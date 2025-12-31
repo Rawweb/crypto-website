da7679a660e2ba150d59237ae379fbb6ed5595ee
rawfile <rawfile.webdev@gmail.com>
2025-12-31 14:26:02 +0100

    resend email setup

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
   try {
     await resend.emails.send({
-      from: 'RawCrypto <onboarding@resend.dev>',
+      from: 'RawCrypto <no-reply@send.rawfilemedia.com>',
       to,
       subject,
       html,
